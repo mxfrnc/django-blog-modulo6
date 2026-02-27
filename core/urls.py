7cdf4e84d065169f9b345e139d1feb231024b35f
Max Franco <maxfr@gmail.com>
2026-02-26 21:01:49 -0300

    Repositorio limpio sin archivos innecesarios

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('post/<slug:slug>/', views.detalle_post, name='detalle_post'),
-    path('crear/', views.crear_post, name='crear_post'),
-    path('nosotros/', views.nosotros, name='nosotros'),
-]
